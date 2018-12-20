6136054cde4e38c800354b366194669590001108
JhunHaoJhan <37074015+JhunHaoJhan@users.noreply.github.com>
2018-12-20 22:17:41 +0800

    Add files via upload

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -2,8 +2,8 @@ test_path=$1
 dict_path=$2
 output_path=$3
 
-wget 'https://www.dropbox.com/s/1vnrv1mys84s4ie/weights-improvement-003-0.7602.hdf5?dl=0'
-wget 'https://www.dropbox.com/s/zco3mt4tofszax2/weights-improvement-002-0.7603.hdf5?dl=0'
-wget 'https://www.dropbox.com/s/qfwf7f88pqdiah1/weights-improvement-003-0.7641.hdf5?dl=0'
+wget 'https://www.dropbox.com/s/1vnrv1mys84s4ie/weights-improvement-003-0.7602.hdf5'
+wget 'https://www.dropbox.com/s/zco3mt4tofszax2/weights-improvement-002-0.7603.hdf5'
+wget 'https://www.dropbox.com/s/qfwf7f88pqdiah1/weights-improvement-003-0.7641.hdf5'
 
 python3 hw4.py --test --test_path $test_path --dict_path $dict_path --output_path $output_path
